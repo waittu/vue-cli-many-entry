66d006d847ef7a73564bd685a5fd64ee438a802a
tuyican <waittutu@163.com>
2019-05-08 16:46:04 +0800

    add HTML模板的多变性

--- many/getPages.js
+++ many/getPages.js
@@ -1,16 +1,19 @@
 const glob = require('glob')
-let pages = {}
-module.exports.pages = function (){
-    glob.sync( './src/views/*/*.js').forEach(filepath =>
-    {
-        console.log(filepath)
+const fs = require('fs')
+module.exports.pages =  ()=>{
+    let pages = {}
+    glob.sync( './src/views/*/*.js').forEach(filepath =>{
         let fileList = filepath.split('/');
         let fileName = fileList[fileList.length-2];
+        let templateHtml = ''
+        fs.exists(`src/views/${fileName}/${fileName}.html`,(data)=>{
+            templateHtml =data?`src/views/${fileName}/${fileName}.html`: 'index.html'
+        })
+        console.log(fileName)
         pages[fileName] = {
             entry: `src/views/${fileName}/${fileName}.js`,
-            // 模板来源
-            template: `index.html`,
-            // 在 dist/index.html 的输出
+            template: templateHtml,
+            // 输出文件路径
             filename: process.env.NODE_ENV === 'development'?`${fileName}.html`:`${fileName}/${fileName}.html`,
             // 提取出来的通用 chunk 和 vendor chunk。
             chunks: ['chunk-vendors', 'chunk-common', fileName]
