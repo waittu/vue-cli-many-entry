d76bc3700b39153ce57025841ac16ba4b9fe95c9
tuyican <waittutu@163.com>
2019-05-09 10:33:59 +0800

    update 入口文件配置问题

--- many/getPages.js
+++ many/getPages.js
@@ -5,11 +5,6 @@ module.exports.pages =  ()=>{
     glob.sync( './src/views/*/*.js').forEach(filepath =>{
         let fileList = filepath.split('/');
         let fileName = fileList[fileList.length-2];
-        // let  htmlPath = `./src/views/${fileName}/${fileName}.html`
-        // let templateHtmlPath = ""
-        // fs.access(htmlPath, fs.constants.F_OK, (err) => {
-        //     templateHtmlPath = err?'index.html':templateHtmlPath
-        // });
         pages[fileName] = {
             entry: `./src/views/${fileName}/${fileName}.js`,
             template: 'index.html',
@@ -18,7 +13,6 @@ module.exports.pages =  ()=>{
             // 提取出来的通用 chunk 和 vendor chunk。
             chunks: ['chunk-vendors', 'chunk-common', fileName]
         }
-        console.log(pages)
     })
     return pages
 };
