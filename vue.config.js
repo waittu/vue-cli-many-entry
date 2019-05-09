cc7cc9c0b18992f96990cb41c403477335938ae3
tuyican <waittutu@163.com>
2019-05-09 09:58:45 +0800

    add less 配置

--- vue.config.js
+++ vue.config.js
@@ -3,9 +3,7 @@ function resolve(dir) {
     return path.join(__dirname, dir);
 }
 const pageMethod  = require('./many/getPages').pages()
-//const publicPath = process.env.NODE_ENV === "production" ? "../" : "/";
 module.exports = {
-    //publicPath:publicPath,
     // 基本配置
     configureWebpack:{
         resolve:{
